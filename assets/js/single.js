9a60cd9a15dbcd0ae29fe3579a0906e232ca28a1
light <light2adrifter@yahoo.com>
2021-11-30 04:58:22 -0500

    dynamic search

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,35 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl= document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name")
+
+var getRepoName = function() {
+    // grabs repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=") [1];
+
+    if (repoName) {
+        // displays repo name on page
+        repoNameEl.textContent= repoName;
+
+        getRepoIssues(repoName)
+    } else {
+        // if no repo was given, directs to homepage
+        document.location.replace("./index.html");
+    }
+
+}
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent= "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // append to container
+    limitWarningEl.appendChild(linkEl);
+};
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,9 +39,15 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data){
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with your request!")
+            // if not succesful returns to homepage
+            document.location.replace("./index.html")
         }
     });
 }
@@ -48,4 +85,5 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("light2adrifter/git-it-done")
+
+getRepoName(repoNameEl);
